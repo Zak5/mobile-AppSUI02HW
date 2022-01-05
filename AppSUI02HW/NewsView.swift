88dca0cacf0b61251a94a6c8a6eef1bef11fe9bd
Zakk <k.zaharev@gmail.com>
2022-01-05 23:28:24 +0300

    Add animation on double tap

--- AppSUI02HW/NewsView.swift
+++ AppSUI02HW/NewsView.swift
@@ -13,7 +13,7 @@ import Navigation
 struct NewsView: View {
     
     @StateObject var newsViewModel = NewsViewModel()
-
+    
     var body: some View {
         VStack {
             CustomPickerView(categories: $newsViewModel.categories, selected: $newsViewModel.category) {
@@ -33,29 +33,61 @@ struct NewsArticlesListView: View {
     
     @ObservedObject var newsViewModel: NewsViewModel
     @EnvironmentObject var routeModel: NavigationContainerViewModel
-
+    @State var selectedArticle: Article?
+    @State var selectedArticleId: String = ""
+    @Namespace private var ns
+    
     var body: some View {
-        List {
-            ForEach(newsViewModel.articles) { article in
-                NewsArticleCell(article: article).onTapGesture {
-                    routeModel.push(screenView: LazyView(ArticleView(article: article)).toAnyView())
+        ZStack(alignment: .bottom){
+            List {
+                ForEach(newsViewModel.articles) { article in
+                    NewsArticleCell(article: article)
+                        .frame(maxHeight: 100)
+                        .matchedGeometryEffect(id: article.id, in: ns, isSource: true)
+                        .onTapGesture(count: 2) {
+                            selectedArticle = article
+                            selectedArticleId = article.id
+                        }
+                        .onTapGesture(count: 1) {
+                            routeModel.push(screenView: LazyView(ArticleView(article: article)).toAnyView())
+                        }
+                }
+            }
+            .listStyle(.plain)
+            
+            if let article = selectedArticle {
+                HStack {
+                    Spacer()
+                    NewsArticleCell(article: article)
+                        .matchedGeometryEffect(id: selectedArticleId, in: ns, isSource: false)
+                        .offset(x: 200, y: 100)
+                        .frame(maxHeight: 100)
+                        .scaleEffect(selectedArticleId == "" ? 0.5 : 1)
+                        .frame(width: selectedArticleId == "" ? 300 : nil)
+                        .onAppear {
+                            withAnimation(Animation.easeInOut(duration: 1.0)) {
+                                selectedArticleId = ""
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(250)) {
+                                selectedArticle = nil
+                            }
+                        }
+                    
                 }
             }
         }
-        .listStyle(.plain)
     }
 }
 
 struct NewsArticleCell: View {
     
     var article: Article
-
+    
     var body: some View {
         Text(article.title ?? "")
     }
     
 }
-
 
 struct NewsCategoriesListView_Previews: PreviewProvider {
     static var previews: some View {
